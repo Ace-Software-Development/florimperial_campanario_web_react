42b41961a13c6ad8643cf1b26dda861855fbb58d
RafaelHinojosa <rafael.zeo@gmail.com>
2022-04-22 00:25:45 -0500

    Libraries installed

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import {
 } from "react-router-dom";
 import Home from './routes/Home';
 import Auth from './routes/Auth';
+import GolfSalidas from './routes/GolfSalidas';
 
 export default function App() {
   return (
     <Router>
       <Switch>
+          <Route path="/golf/salidas">
+            <GolfSalidas />
+          </Route>
           <Route path="/home">
             <Home />
           </Route>
