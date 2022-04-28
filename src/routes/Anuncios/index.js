d742fc635152692487e43b8174c5fc8f71b7cf5c
cutberto <cutbertoan@gmail.com>
2022-04-28 11:21:23 -0500

    FEAT/Anuncios terminados

--- src/routes/Anuncios/index.js
+++ src/routes/Anuncios/index.js
@@ -8,37 +8,30 @@ import ParseObject from "parse/lib/browser/ParseObject";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useParams,
-  useRouteMatch
-} from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Modal from "react-bootstrap/Modal";
 import Spinner from "react-bootstrap/Spinner";
 import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
-import { useForm } from "react-hook-form";
+import { confirmAlert } from 'react-confirm-alert'; // Import
+import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
+
 
 
 export default function Anuncios() {
   const history = useHistory();
-  const [anuncioTitle, setAnuncioTitle] = useState("");
-  const [anuncioContent, setAnuncioContent] = useState("");
   const [anuncios, setAnuncios] = useState(null);
   const [loading, setLoading] = useState(true);
   const [show, setShow] = useState(false);
+  const [selectedId, setSelectedId] = useState(null);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  const [showMoreInfo, setShowMoreInfo] = useState(false);
-  let { path, url } = useRouteMatch();
+  
   const [validated, setValidated] = useState(false);
   const [buttonDisabled, setButtonDisabled] = useState(false);
+
+
 
   async function getAnuncios() {
     const query = new Parse.Query("Anuncio");
@@ -48,13 +41,13 @@ export default function Anuncios() {
     const result = new Array();
     for (var i = 0; i < anuncios.length; i++) {
       console.log(anuncios[i].get("titulo"));
-
+      const fecha = new Date(anuncios[i].get("updatedAt").toString());
       result.push(
         new Array(
           anuncios[i].get("titulo"),
           anuncios[i].get("contenido"),
           anuncios[i].get("imagen").url(),
-          anuncios[i].get("updatedAt"),
+          fecha.getDate() + "/" + (fecha.getMonth() +1) + "/" + fecha.getFullYear(),
           anuncios[i].id
         )
       );
@@ -108,71 +101,75 @@ export default function Anuncios() {
     };
   }
 
-  const handleSubmitPost = (e) => {
-    e.preventDefault();
-    const Anuncio = Parse.Object.extend("Anuncio");
-    const fileUploadControl = document.getElementById("formImg").files[0];
+  const confirmDelete = (ObjId) => {
+    
 
-    console.log("imprimiendo register:", fileUploadControl);
-    const img64 = getBase64(fileUploadControl); // prints the base64 string<
-    const img2 = new Parse.File(`img-${anuncioTitle}`, fileUploadControl);
-    img2.save().then(
-      function () {
-        // The file has been saved to Parse.
-        const newAnuncio = new Anuncio();
-        newAnuncio
-          .save({
-            titulo: anuncioTitle,
-            contenido: anuncioContent,
-            imagen: img2,
-          })
-          .then(
-            (newAnuncio) => {
-              // Execute any logic that should take place after the object is saved.
-              alert("New object created with objectId: " + newAnuncio.id);
-            },
-            (error) => {
-              // Execute any logic that should take place if the save fails.
-              // error is a Parse.Error with an error code and message.
-              alert(
-                "Failed to create new object, with error code: " + error.message
-              );
-              console.log(error.message);
-            }
-          );
-        setAnuncioContent("");
-        setAnuncioTitle("");
-        handleClose();
-      },
-      function (error) {
-        // The file either could not be read, or could not be saved to Parse.
-        alert("the parse file could not be created, with error: ", error);
-      }
-    );
+   
 
-     //window.location.reload();
-  };
+    confirmAlert({
+      title: 'Eliminar anuncio',
+      message: '¿Estas seguro que deseas eliminar este anuncio?',
+      buttons: [
+        {
+          label: 'No',
+          onClick: () => {}
+        },
+        {
+          label: 'Sí',
+          onClick: () => {
+            var yourClass = Parse.Object.extend("Anuncio");
+            var query = new Parse.Query(yourClass);
+            
+            query.get(ObjId)
+            .then((yourObj) => {
+                yourObj.destroy()
+                .then( () =>{
+                  window.location.reload();
+                }
+
+                );
+               // window.location.reload();
+              },
+              (error) => {
+                // The object was not retrieved successfully.
+                // error is a Parse.Error with an error code and description.
+              }
+            ); 
+            } 
+        }
+        
+      ]
+    });
+  }
 
   console.log(anuncios);
   const listItems = anuncios.map((anuncio) => (
     <Col >
       <Card
-        className="bg-image text-white"
+        className=" text-white"
         style={{
           width: "100%",
-          height: "12rem",
-          borderRadius: "5%",
-          background: `rgba(0, 0, 0, .65) url(${anuncio[2]})`,
-          "background-blend-mode": "darken"
+          height: "32rem",
+          borderRadius: "5%"
+          , "background-color": "#2B4066"
         }}
-      >
-        <Card.Body
-          className="d-flex align-items-end"
-          style={{ "text-shadow": "1px 1px #000000" }}
-        >
-          <Card.Text>{anuncio[0]}</Card.Text>
-         </Card.Body>
-        <a href={`${url}/${anuncio[4]}`} className="stretched-link"></a>
+      > 
+      <div class ="d-flex justify-content-center">    
+      
+      <Card.Img variant="top" src={`${anuncio[2]}`} style={{borderRadius:"5%",  "maxHeight": "27rem" , "maxWidth":"80%"  }} />
+      </div>
+ 
+      
+         <Card.Footer className="d-flex justify-content-between mt-auto">  
+            <h5> {anuncio[3]} </h5> 
+            <Button variant="primary" class="btn-trash" className="btn-campanario" onClick= {() => {  confirmDelete(anuncio[4]);}}  >
+              <ion-icon name="trash-outline" />
+            </Button>  
+            
+          </Card.Footer>
+
+
+        
       </Card>
     </Col>
   ));
@@ -194,7 +191,7 @@ export default function Anuncios() {
   
       console.log("imprimiendo register:", fileUploadControl);
       const img64 = getBase64(fileUploadControl); // prints the base64 string<
-      const img2 = new Parse.File(`img-${anuncioTitle}`, fileUploadControl);
+      const img2 = new Parse.File(`img-anuncio`, fileUploadControl);
       setButtonDisabled(true); // <-- disable the button here
 
       img2.save().then(
@@ -203,9 +200,9 @@ export default function Anuncios() {
           const newAnuncio = new Anuncio();
           newAnuncio
             .save({
-              titulo: anuncioTitle,
-              contenido: anuncioContent,
-              imagen: img2,
+           /*   titulo: anuncioTitle,
+              contenido: anuncioContent,*/
+              imagen: img2
             })
             .then(
               (newAnuncio) => {
@@ -225,10 +222,8 @@ export default function Anuncios() {
                 console.log(error.message);
               }
             );
-          setAnuncioContent("");
-          setAnuncioTitle("");
+       
           handleClose();
-        //  window.location.reload();
           setValidated(true);
         },
         function (error) {
@@ -243,53 +238,19 @@ export default function Anuncios() {
           
 
     };
-  
+    
     return (
       <Form noValidate validated={validated} onSubmit={handleSubmit}   id = "createAnnouncementForm">
         <Row className="mb-3">
        
 
-          <Form.Group as={Col} md="12" controlId="formTitulo">
-            <Form.Label>Título del anuncio</Form.Label>
-            <InputGroup hasValidation>
-               <Form.Control
-                type="text"
-                placeholder="Ingresa el título del anuncio"
-                required
-                onChange={(event) =>
-                  setAnuncioTitle(event.currentTarget.value)
-                  
-                }
-              />
-              
-              <Form.Control.Feedback type="invalid">
-                Por favor introduce un título para el anuncio.
-              </Form.Control.Feedback>
-            </InputGroup>
-          </Form.Group>
-        </Row>
-        <Row className="mb-3">
-          <Form.Group as={Col} md="12" controlId="formContenido">
-            <Form.Label>Contenido del anuncio</Form.Label>
-            <Form.Control 
-            as="textarea"
-            type="text" 
-            placeholder="Ingresa aquí el contenido del anuncio." 
-            required 
-            onChange={(event) =>
-              setAnuncioContent(event.currentTarget.value)
-            }
-            />
-            <Form.Control.Feedback type="invalid">
-              Por favor ingresa el contenido del anuncio aquí.
-            </Form.Control.Feedback>
-          </Form.Group>
+         
           <Form.Group as={Col} md="12" controlId="formImg">
-            <Form.Label>Agregar una imagen para el anuncio</Form.Label>
+            <Form.Label >Agregar una imagen para el anuncio</Form.Label>
             <Form.Control 
             type="file" 
             id = "formImg"
-            accept ="image/png" 
+            accept ="image/*" 
             required />
             <Form.Control.Feedback type="invalid">
               Por favor sube aquí la imagen que se agregará al anuncio.
@@ -304,6 +265,7 @@ export default function Anuncios() {
   
   const testform = FormExample();
 
+  
 
   return (
     <div className="App">
@@ -324,10 +286,10 @@ export default function Anuncios() {
 
                       </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
+            <Button className="btn-campanario" onClick={handleClose}>
               Cancelar
             </Button>
-            <Button variant="primary" type="submit" form="createAnnouncementForm"  disabled ={buttonDisabled}>
+            <Button type="submit" form="createAnnouncementForm" className="btn-publicar" disabled ={buttonDisabled}>
               Publicar
             </Button>
           </Modal.Footer>
@@ -338,17 +300,17 @@ export default function Anuncios() {
         <Row xs={1} s={2} md={3} className="g-4">
           <Col>
             <Card
-              className=" text-align-center justify-content-md-center"
-              style={{ width: "100%", height: "12rem", borderRadius: "5%" }}
+              className=" d-flex text-align-center justify-content-md-center"
+              style={{ width: "100%", height: "32rem", borderRadius: "5%" }}
               onClick={handleShow}
             >
-              <Card.Title className="text-center ">
+              <Card.Title className="text-center">
                 {" "}
                 <br /> Agregar un anuncio
 
               </Card.Title>
-              <div style={{width:"100%", height: "100%"}}>
-                <ion-icon name="add-circle-outline"   style={{ width: "30%", height: "50%" }}></ion-icon>
+              <div className="d-flex">
+                <ion-icon name="add-circle-outline" class="icon-plus"  style={{ width: "100%", height: "100%" }}></ion-icon>
                 </div>
 
             
