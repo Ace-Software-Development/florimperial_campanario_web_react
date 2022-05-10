ddd9d7b528a126c0c3effe255ef430aef58cf948
rafael_hinojosa <rafael.zeo@gmail.com>
2022-05-10 16:12:12 -0500

    Salidas Golf init

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ import Home from './pages/Home';
 import Auth from './pages/Auth';
 import Anuncios from './pages/Anuncios'
 import DetalleAnun from './pages/DetalleAnun'
+import SalidasGolf from './pages/Golf/SalidasGolf'
+
 export default function App() {
   return (
     <Router>
       <Switch>
-        
           <Route path="/home">
             <Home />
           </Route>
-
           <Route path= "/anuncios/:anuncioId" render={()=> {
            return (
             <div>
@@ -26,7 +26,9 @@ export default function App() {
          }
          }    
          />
-        
+        <Route path="/golf/salidas">
+            <SalidasGolf />
+        </Route>
           <Route path="/anuncios">
             <Anuncios />
           </Route>
---
+++ src/pages/Golf/SalidasGolf.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+import Parse from 'parse';
+import FullCalendar from "@fullcalendar/react";
+import daygridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+export default function SalidasGolf() {
+    return (
+        <div> Hola </div>
+    );
+}
