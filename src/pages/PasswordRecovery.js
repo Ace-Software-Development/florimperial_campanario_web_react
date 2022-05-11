bd901c4ab57390ac96edfae5cfca00420e0c7239
cutberto <cutbertoan@gmail.com>
2022-05-11 17:37:35 -0500

    FEAT/Refactor al front de recuperar contraseña

--- src/pages/PasswordRecovery.js
+++ src/pages/PasswordRecovery.js
@@ -6,7 +6,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import "../css/DetalleAnun.css";
+import "../css/Anuncios.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
@@ -53,51 +53,46 @@ export default function PasswordRecovery() {
         <Row xs={12} s={12} md={12} className="g-4">
           <Col className="d-flex justify-content-center"  >
 
-          <Card
-            className=" d-flex text-align-center justify-content-center"
-            style={{
-              width: "80%",
-              height: "32rem",
-              borderRadius: "5%"
-             
-            }}
-          >
-            <a href= "/" > <ion-icon name="arrow-back-outline" className="arrow-icon" ></ion-icon></a>
-            <Card.Body>
-            <Card.Text  className=" d-flex text-align-center justify-content-center"> 
-              <h3> Por favor, ingrese su correo electrónico. Recibirá una liga para recuperar su contraseña </h3>
-              </Card.Text>
+          
+          
 
+  
+       
+          <Card className="text-center position-relative" style={{width:"50%"}}  >
+          <a href= "/" className="position-absolute top-0 start-0 " > <ion-icon name="arrow-back-outline" className="arrow-icon" ></ion-icon></a>
+
+             <Card.Body>
+              <br/><br/>
+              <Card.Text>Por favor, ingrese su correo electrónico. Recibirá una liga para recuperar su contraseña</Card.Text>
+              <br/><br/>
+              
               <Form id = "recoverPassword"  onSubmit={handleSubmit}   >
                 <Row className="mb-3">
-              
-
-                
                   <Form.Group as={Col} md="12">
                     <Form.Control 
                     type="email" 
                     id = "email"
                     required 
                     onChange={event => setEmail(event.currentTarget.value)}
+                 
                     />
-                    
+                        
                   </Form.Group>
-                
+                    
                 </Row>
 
-            </Form>
-            
-            </Card.Body>
-         
-            <Button  type="submit" form="recoverPassword" className="btn-publicar" > Recuperar contraseña </Button>
-           
-           
-              
-           
-          </Card>
+              </Form>
+              <br/>
+                
+              <Button variant="primary" type="submit" form="recoverPassword" className="btn-publicar"  > Recuperar contraseña </Button>
+              <br/><br/>
 
-          </Col>
-       
+
+            </Card.Body>
+ 
+           </Card>
+
+           </Col>
         </Row>
 
 
