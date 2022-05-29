dd78f5f97c804d896f1216686f2d36bdadcea299
cutberto <cutbertoan@gmail.com>
2022-05-28 21:47:20 -0500

    feat/implementar tarjetas de home con roles

---
+++ src/components/HomeIcons.jsx
@@ -0,0 +1,118 @@
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import '../css/Dashboard.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Card from 'react-bootstrap/Card';
+import React from "react"
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+
+
+function HomeIcons(permissions){
+    let anuncios, golf, raqueta, salones_gym, gestion, alberca = "";
+    if(permissions.permissions.Anuncios === false){
+        anuncios = "none";
+    }
+    if(permissions.permissions.Raqueta === false){
+        raqueta = "none";
+    }
+    if(permissions.permissions.Salones_gym === false){
+        salones_gym = "none";
+    }
+    if(permissions.permissions.Golf === false){
+        golf = "none";
+    }
+    if(permissions.permissions.Gestion === false){
+        gestion = "none";
+    }
+    if(permissions.permissions.Alberca === false){
+        alberca = "none";
+    }
+
+    return(
+    <div className="d-flex">
+        <Container>
+            <Row xs={1} s={2} md={3} className="g-4">
+                <Col>
+                    <Card className="bg-dark text-white" style={{width: "100%",display: `${anuncios}` }}>
+                        <Card.Img src="holder.js/100px270" alt="Card image" />
+                        <Card.ImgOverlay>
+                            <Card.Title>Anuncios</Card.Title>
+                        </Card.ImgOverlay>
+                    </Card> 
+                </Col>
+                <Col>
+                    <Card className="bg-dark text-white" style={{width: "100%",display: `${golf}`}}>
+                        <Card.Img src="holder.js/100px270" alt="Card image" />
+                        <Card.ImgOverlay>
+                            <Card.Title>Golf</Card.Title>
+                        </Card.ImgOverlay>
+                    </Card> 
+                </Col>
+                <Col>
+                <Card className="bg-dark text-white" style={{width: "100%",display: `${raqueta}`}}>
+                        <Card.Img src="holder.js/100px270" alt="Card image" />
+                        <Card.ImgOverlay>
+                            <Card.Title>Raqueta</Card.Title>
+                        </Card.ImgOverlay>
+                    </Card> 
+                </Col>
+                <Col>
+                    <Card className="bg-dark text-white" style={{width: "100%",display: `${gestion}`}}>
+                        <Card.Img src="holder.js/100px270" alt="Card image" />
+                        <Card.ImgOverlay>
+                            <Card.Title>Gestión de socios</Card.Title>
+                        </Card.ImgOverlay>
+                    </Card> 
+                </Col>
+                <Col>
+                <Card className="bg-dark text-white" style={{width: "100%",display: `${gestion}`}}>
+                        <Card.Img src="holder.js/100px270" alt="Card image" />
+                        <Card.ImgOverlay>
+                            <Card.Title>Gestión de administradores</Card.Title>
+                        </Card.ImgOverlay>
+                    </Card> 
+                </Col>
+                <Col>
+                    <Card className="bg-dark text-white" style={{width: "100%",display: `${salones_gym}`}}>
+                        <Card.Img src="holder.js/100px270" alt="Card image" />
+                        <Card.ImgOverlay>
+                            <Card.Title>Gym</Card.Title>
+                        </Card.ImgOverlay>
+                    </Card>           
+                </Col>
+                <Col>
+                    <Card className="bg-dark text-white" style={{width: "100%", display: `${salones_gym}`}}>
+                        <Card.Img src="holder.js/100px270" alt="Card image" />
+                        <Card.ImgOverlay>
+                            <Card.Title>Salones</Card.Title>
+                        </Card.ImgOverlay>
+                    </Card> 
+                </Col>
+                <Col>
+                    <Card className="bg-dark text-white" style={{width: "100%", display: `${alberca}`}}>
+                        <Card.Img src="holder.js/100px270" alt="Card image" />
+                        <Card.ImgOverlay>
+                            <Card.Title>Alberca</Card.Title>
+                        </Card.ImgOverlay>
+                    </Card> 
+                </Col>
+            </Row>
+
+
+      </Container>
+
+           
+           
+          
+
+        </div>
+    )
+}
+
+export default HomeIcons
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,38 +4,19 @@ import Parse from 'parse';
 import '../css/Home.css'
 import {useParseQuery} from '@parse/react';
 import ParseObject from 'parse/lib/browser/ParseObject';
-import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
 import SidenavOverlay from '../components/SidenavOverlay';
 import {getPermissions} from '../utils/client'
 import CirculoCarga from "../components/CirculoCarga";
-
+import HomeIcons from '../components/HomeIcons'
 
 export default function Home() {
   const history = useHistory();
-  const [postText, setPostText] = useState('');
-  const [anuncios, setAnuncios] = useState(null);
   const [loading, setLoading] = useState(true);
   const [permissions, setPermissions] = useState({});
 
-  async function getAnuncios() {
-    const query = new Parse.Query('Anuncio');
-    // query donde no esten eliminados
-    const anuncios = await query.find();
-    const titleArray = new Array();
-    const contentArray = new Array();
-    for (var i = 0; i < anuncios.length; i++){
-    //  console.log(anuncios[i].get("titulo"));
-      titleArray.push(anuncios[i].get("titulo"));
-      contentArray.push(anuncios[i].get("contenido"));
-    }
-    const result = new Array();
-    result.push(titleArray);
-    result.push(contentArray);
-    return result;
-  }
 
   useEffect(async() => {
     async function checkUser() {
@@ -64,15 +45,11 @@ export default function Home() {
       }
     
     }
-    
-    const permissionsJson = await checkUser();
-    setPermissions(permissionsJson);
-  
 
     try {
       setLoading(true);
-      const anuncios = await getAnuncios();
-      setAnuncios(anuncios);
+      const permissionsJson = await checkUser();
+      setPermissions(permissionsJson);
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -80,76 +57,21 @@ export default function Home() {
     }
   }, []);
 
-  console.log(permissions);
-  // if (permissions.Alberca === true)  {
-  //   history.push('/');
-  // };
-  // return a Spinner when loading is true
   if(loading) return (
     <span><CirculoCarga/></span>
   );
-/*
-  // data will be null when fetch call fails
-  if (!anuncios) return (
-    <span>Data not available</span>
-  );
 
-  // when data is available, title is shown
-  return (
-    <span>
-      {anuncios[0].get("title")}
-    </span>
-  );
-*/
-
-//console.log(anuncios[0]);
-  
-  const handleSubmitPost = (e) => {
-    e.preventDefault();
-    const Post = Parse.Object.extend("Post");
-    const newPost = new Post();
-    newPost.save()
-    .then((newPost) => {
-      // Execute any logic that should take place after the object is saved.
-      alert('New object created with objectId: ' + newPost.text);
-    }, (error) => {
-      // Execute any logic that should take place if the save fails.
-      // error is a Parse.Error with an error code and message.
-      alert('Failed to create new object, with error code: ' + error.message);
-    });
-    setPostText("");
-  };
-
-  // var items =  new Array();  
-  // items = anunciosGlobal.map((anuncio) =>
-  //   <li>{ anuncio }</li>
-  // );
-  
-  const listItems = anuncios.map((anuncio) =>
-  <Card style={{ width: '18rem' }}>
-     <Card.Img variant="top" src="logo512.png" />
-    <Card.Body>
-    <Card.Title>
-    {anuncio[0]}
-    </Card.Title>
-    <Card.Text>
-    {anuncio[1]}  
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-    </Card.Body>
- </Card>
-);
 
   return (
     <div className="App">
       <Sidebar permissions = {permissions} />
       <Header processName={"Inicio"}/>
+
+      <div style ={{"margin-left" :"100px" }}>
+        <HomeIcons permissions = {permissions}  />
+      </div>
       
-      <div className="posts-container">
-        {listItems}
-      </div>
-      <div>
-      </div>
+      
     </div>
   );
 }
