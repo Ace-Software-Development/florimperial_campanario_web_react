3bcb9b18544a4c6e38356aceeaf69c6c8e8c1921
Julio César  Gómez González <a01274966@itesm.mx>
2022-06-03 19:06:08 -0500

    Font mi perfil

--- src/pages/MiPerfil.js
+++ src/pages/MiPerfil.js
@@ -1,3 +1,4 @@
+import '../css/MiPerfil.css';
 import React from 'react';
 import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 import {useEffect, useState} from 'react';
@@ -8,6 +9,9 @@ import Screen from '../components/Screen';
 import Card from 'react-bootstrap/card';
 import Parse from 'parse';
 import ParseObject from 'parse/lib/browser/ParseObject';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function MiPerfil() {
   const history = useHistory();
@@ -59,14 +63,17 @@ export default function MiPerfil() {
   return (
     <Screen permissions={permissions} title="Mi perfil">
       <div className="App">
-        <div style={{marginLeft: '145px'}}>
-          <Card>
-            Usuario
-            {currentUser.id}
+        <div style={{marginLeft: '15px'}}>
+         <Card className="profile-card">
+            <Container className= "profile-container">
+              <Row xs={2} md={4} lg={6}>
+                <Col sm><ion-icon name="person-circle-outline" style={{fontSize: '10em'}}></ion-icon></Col>
+                <Col sm><h6>Usuario:</h6> El que no debe ser nombrado{currentUser.id} <br/><br/><h6>Rol actual:</h6>{currentRole.id}</Col>
+                <Col sm><h6>Correo electrónico:</h6> correo@ejemplo.com <br/><br/><h6>Numero de nómina:</h6>123456789</Col> 
+              </Row>
+            </Container>
+            </Card>
             <br />
-            {currentRole.id}
-            <ion-icon name="person-circle-outline"></ion-icon>
-          </Card>
         </div>
       </div>
     </Screen>
