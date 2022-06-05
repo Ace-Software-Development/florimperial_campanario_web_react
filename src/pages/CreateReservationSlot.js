02ac6f83a29f3fcfe5606ddbab179163f1605578
Luis-ERP <edgar.ramirez@shopventory.com>
2022-06-05 00:31:20 -0500

    W18 arreglado un bug que no permitia crear una reservacion

--- src/pages/CreateReservationSlot.js
+++ src/pages/CreateReservationSlot.js
@@ -4,12 +4,14 @@ import DialogTitle from '@mui/material/DialogTitle';
 import { DialogContent, DialogActions } from '@mui/material';
 import { Button } from '@mui/material';
 import { getAllCoaches, createGolfReservation, getAreaByName, getSitiosByArea } from '../utils/client';
+import CirculoCarga from '../components/CirculoCarga';
 import InputSelector from '../components/InputSelector';
 import Datetime from 'react-datetime';
 import "react-datetime/css/react-datetime.css";
 
 
 export default function CreateGolfAppointmentSlot(props) {
+    const [loading, setLoading] = useState(true);
 	const [disabledButton, setDisabledButton] = useState(false);
     const [appointment, setAppointment] = useState({
         'fechaInicio': props.startingDate,
@@ -27,7 +29,9 @@ export default function CreateGolfAppointmentSlot(props) {
     });
 
     useEffect(() => {
-        if (props.sitios && props.sitios.length===1) {
+        if (props.sitios) {
+            console.log(props.sitios);
+            setLoading(false);
             const sitioNewData = {
                 'objectId': props.sitios[0].objectId,
                 'nombre': props.sitios[0].nombre,
@@ -35,7 +39,7 @@ export default function CreateGolfAppointmentSlot(props) {
             };
             setAppointment({...appointment, sitio: sitioNewData});
         }
-    }, []);
+    }, [props.sitios]);
 
     const handleClose = () => {
         props.onClose(false);
@@ -59,112 +63,117 @@ export default function CreateGolfAppointmentSlot(props) {
         return true;
     }
 
-    return(
-        <Dialog open={props.open} onClose={handleClose}>
-            <DialogTitle>Nuevo espacio de reservación</DialogTitle>
-            <DialogContent>
+    if (!loading)
+        return (
+            <Dialog open={props.open} onClose={handleClose}>
+                <DialogTitle>Nuevo espacio de reservación</DialogTitle>
+                <DialogContent>
 
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <p>Fecha y hora</p>
-                                </td>
-                                <td>
-                                    <Datetime
-                                        inputProps={{className:'input'}}
-                                        initialValue={props.startingDate}
-                                        input={false}
-                                        onChange={date => appointmentOnChange('reservacion', 'fechaInicio', new Date(date.toISOString()))} 
-                                    />
-                                </td>
-                            </tr>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <p>Fecha y hora</p>
+                                    </td>
+                                    <td>
+                                        <Datetime
+                                            inputProps={{className:'input'}}
+                                            initialValue={props.startingDate}
+                                            input={false}
+                                            onChange={date => appointmentOnChange('reservacion', 'fechaInicio', new Date(date.toISOString()))} 
+                                        />
+                                    </td>
+                                </tr>
 
-                            {props.sitios && props.sitios.length > 1 &&
-								<tr>
-									<td>
-										<p>Sitio de salida</p>
-									</td>
-									<td>
-										{props.sitios.map(sitio => {
-											return(
-												<div key={`${sitio.objectId}-sitio-div`}>
-													<input
-                                                        key={`${sitio.objectId}-sitio-input`}
-														type="radio"
-														id={sitio.objectId}
-														value={sitio.objectId}
-														name="sitio"
-														defaultChecked={appointment.sitio.objectId === sitio.objectId}
-														onChange={event => appointmentOnChange('sitio', {nombre: sitio.nombre, objectId: event.target.value, tableName: 'Sitio'})}
-													/>
-													<label htmlFor={sitio.objectId}>{sitio.nombre}</label>
-												</div>
-												);
-											})
-										}
-									</td>
-								</tr>
-							}
+                                {props.sitios && props.sitios.length > 1 &&
+                                    <tr>
+                                        <td>
+                                            <p>Sitio de salida</p>
+                                        </td>
+                                        <td>
+                                            {props.sitios.map((sitio, index) => {
+                                                return(
+                                                    <div key={`${sitio.objectId}-sitio-div`}>
+                                                        <input
+                                                            key={`${sitio.objectId}-sitio-input`}
+                                                            type="radio"
+                                                            id={sitio.objectId}
+                                                            value={sitio.objectId}
+                                                            name="sitio"
+                                                            defaultChecked={index === 0}
+                                                            onChange={event => appointmentOnChange('sitio', {nombre: sitio.nombre, objectId: event.target.value, tableName: 'Sitio'})}
+                                                        />
+                                                        <label htmlFor={sitio.objectId}>{sitio.nombre}</label>
+                                                    </div>
+                                                    );
+                                                })
+                                            }
+                                        </td>
+                                    </tr>
+                                }
 
-                            <tr>
-                                <td>
-                                    <p>Máximo asistentes</p>
-                                </td>
-                                <td>
-                                    <input
-                                        className='input'
-                                        type="number"
-                                        min="1"
-                                        value={appointment.maximoJugadores}
-                                        onChange={event => {
-                                            if (event.target.value)
-                                                return appointmentOnChange('reservacion', 'maximoJugadores', parseInt(event.target.value));
-                                        }} 
-                                    />
-                                </td>
-                            </tr>
-                            
-							{props.coachInput &&
-								<tr>
-									<td>
-										<p>Coach disponible</p>
-									</td>
-									<td>
-										<InputSelector
-											getDisplayText={i => i.nombre}
-											getElementId={i => i.objectId}
-											placeholder='Nombre del coach'
-											defaultValue={appointment.profesor}
-											onChange={coach => {appointmentOnChange('recervacion', 'profesor', coach)}}
-											getListData={async () => {
-												const response = await getAllCoaches();
-												const data = [];
-												response.forEach(i => {
-													data.push({objectId: i.id, nombre: i.get('nombre'), tableName: 'Profesor'});
-												});
-												return data;
-											}}
-										/>
-									</td>
-								</tr>
-							}
-                        </tbody>
-                    </table>
+                                <tr>
+                                    <td>
+                                        <p>Máximo asistentes</p>
+                                    </td>
+                                    <td>
+                                        <input
+                                            className='input'
+                                            type="number"
+                                            min="1"
+                                            value={appointment.maximoJugadores}
+                                            onChange={event => {
+                                                if (event.target.value)
+                                                    return appointmentOnChange('reservacion', 'maximoJugadores', parseInt(event.target.value));
+                                            }} 
+                                        />
+                                    </td>
+                                </tr>
+                                
+                                {props.coachInput &&
+                                    <tr>
+                                        <td>
+                                            <p>Coach disponible</p>
+                                        </td>
+                                        <td>
+                                            <InputSelector
+                                                getDisplayText={i => i.nombre}
+                                                getElementId={i => i.objectId}
+                                                placeholder='Nombre del coach'
+                                                defaultValue={appointment.profesor}
+                                                onChange={coach => {appointmentOnChange('recervacion', 'profesor', coach)}}
+                                                getListData={async () => {
+                                                    const response = await getAllCoaches();
+                                                    const data = [];
+                                                    response.forEach(i => {
+                                                        data.push({objectId: i.id, nombre: i.get('nombre'), tableName: 'Profesor'});
+                                                    });
+                                                    return data;
+                                                }}
+                                            />
+                                        </td>
+                                    </tr>
+                                }
+                            </tbody>
+                        </table>
 
-                    <DialogActions>
-                        <Button onClick={handleClose}>Cancelar</Button>
-                        <Button onClick={async () => {
-                            if(disabledButton)
-                                return;
-                            const status = await onSubmit();
-                            if (status) {
-                                setDisabledButton(true);
-                                window.location.reload()
-                            }
-                        }} type="submit">Crear</Button>
-                    </DialogActions> 
-            </DialogContent>
-        </Dialog>
-    );
+                        <DialogActions>
+                            <Button onClick={handleClose}>Cancelar</Button>
+                            <Button onClick={async () => {
+                                if(disabledButton)
+                                    return;
+                                const status = await onSubmit();
+                                if (status) {
+                                    setDisabledButton(true);
+                                    window.location.reload()
+                                }
+                            }} type="submit">Crear</Button>
+                        </DialogActions> 
+                </DialogContent>
+            </Dialog>
+        );
+    else 
+        return (
+            <CirculoCarga />
+        );
 }
